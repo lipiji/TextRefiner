ef07cb390687dea513b9ef803b245ef1f8e2b68a
Piji <pagelee.sd@gmail.com>
2015-01-04 21:54:18 +0800

    format

--- src/main/java/com/lipiji/textrefiner/TextRefiner.java
+++ src/main/java/com/lipiji/textrefiner/TextRefiner.java
@@ -3,13 +3,13 @@ package com.lipiji.textrefiner;
 import org.apache.commons.lang3.StringUtils;
 
 public class TextRefiner {
-	private static final String PUNCTUATION = "[\\p{P}+~$`^=|<>～｀＄＾＋＝｜＜＞￥×]";
-	
-	public static String filterPunctuation(String text) {
-		if (StringUtils.isBlank(text)) {
-			return "";
-		} else {
-			return text.replaceAll(PUNCTUATION, "");
-		}
-	}
+    private static final String PUNCTUATION = "[\\p{P}+~$`^=|<>～｀＄＾＋＝｜＜＞￥×]";
+
+    public static String filterPunctuation(String text) {
+        if (StringUtils.isBlank(text)) {
+            return "";
+        } else {
+            return text.replaceAll(PUNCTUATION, "");
+        }
+    }
 }
--- src/test/java/com/lipiji/textrefiner/TestTextRefiner.java
+++ src/test/java/com/lipiji/textrefiner/TestTextRefiner.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestTextRefiner {
-	
-	@Test
-	public void filterPunctuation() {
-		assertEquals("This is a cat", TextRefiner.filterPunctuation("This, is. a cat!$"));
-	}
+
+    @Test
+    public void filterPunctuation() {
+        assertEquals("This is a cat", TextRefiner.filterPunctuation("This, is. a cat!$"));
+    }
 }
