48896e6257381516fd41776710d5e7d1ee9f52d2
Piji <pagelee.sd@gmail.com>
2015-01-14 23:11:09 +0800

    split text to sentences

--- src/main/java/com/lipiji/textrefiner/TextRefiner.java
+++ src/main/java/com/lipiji/textrefiner/TextRefiner.java
@@ -1,5 +1,10 @@
 package com.lipiji.textrefiner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class TextRefiner {
@@ -12,4 +17,18 @@ public class TextRefiner {
             return text.replaceAll(PUNCTUATION, "");
         }
     }
+    
+    //http://stackoverflow.com/a/21430792/160698
+    public static List<String> toSentences(String text) {
+        Pattern re = Pattern.compile("[^.!?\\s][^.!?]*(?:[.!?](?!['\"]?\\s|$)[^.!?]*)*[.!?]?['\"]?(?=\\s|$)", Pattern.MULTILINE | Pattern.COMMENTS);
+        List<String> list = new ArrayList<>();
+        if (StringUtils.isBlank(text)) {
+            return list;
+        }
+        Matcher reMatcher = re.matcher(text);
+        while (reMatcher.find()) {
+            list.add(reMatcher.group());
+        }
+        return list;
+    } 
 }
--- src/test/java/com/lipiji/textrefiner/TestTextRefiner.java
+++ src/test/java/com/lipiji/textrefiner/TestTextRefiner.java
@@ -10,4 +10,10 @@ public class TestTextRefiner {
     public void filterPunctuation() {
         assertEquals("This is a cat", TextRefiner.filterPunctuation("This, is. a cat!$"));
     }
+
+    @Test
+    public void toSentences() {
+        String str = "This is how I tried to split a paragraph into a sentence. But, there is a problem. My paragraph includes dates like Jan.13, 2014 , words like U.S and numbers like 2.2. They all got splitted by the above code.";
+        assertEquals("This is how I tried to split a paragraph into a sentence.", TextRefiner.toSentences(str).get(0));
+    }
 }
