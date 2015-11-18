d026b1f6dd91ec9e499fb2da172054be901ec630
Piji <pagelee.sd@gmail.com>
2015-11-18 17:14:20 +0800

    paragraph 2 sentences using stanford nlp

--- src/main/java/com/lipiji/textrefiner/TextRefiner.java
+++ src/main/java/com/lipiji/textrefiner/TextRefiner.java
@@ -1,11 +1,17 @@
 package com.lipiji.textrefiner;
 
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
+
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.process.DocumentPreprocessor;
 
 public class TextRefiner {
     private static final String PUNCTUATION = "[\\p{P}+~$`^=|<>～｀＄＾＋＝｜＜＞￥×]";
@@ -30,5 +36,17 @@ public class TextRefiner {
             list.add(reMatcher.group());
         }
         return list;
-    } 
+    }
+    
+    public static List<String> toSentenceByStanfordNLP(String text) {
+        Reader reader = new StringReader(text);
+        DocumentPreprocessor dp = new DocumentPreprocessor(reader);
+        List<String> sentenceList = new ArrayList<>();
+
+        for (List<HasWord> sentence : dp) {
+            String sentenceString = Sentence.listToString(sentence);
+            sentenceList.add(sentenceString.toString());
+        }
+        return sentenceList;
+    }
 }
--- src/test/java/com/lipiji/textrefiner/TestTextRefiner.java
+++ src/test/java/com/lipiji/textrefiner/TestTextRefiner.java
@@ -13,7 +13,11 @@ public class TestTextRefiner {
 
     @Test
     public void toSentences() {
-        String str = "This is how I tried to split a paragraph into a sentence. But, there is a problem. My paragraph includes dates like Jan.13, 2014 , words like U.S and numbers like 2.2. They all got splitted by the above code.";
+        String str = "This is how I tried to split a paragraph into a sentence."
+                + " But, there is a problem."
+                + " My paragraph includes dates like Jan.13, 2014, words like U.S and numbers like 2.2."
+                + " They all got splitted by the above code.";
         assertEquals("This is how I tried to split a paragraph into a sentence.", TextRefiner.toSentences(str).get(0));
+        assertEquals("My paragraph includes dates like Jan. 13 , 2014 , words like U.S and numbers like 2.2 .", TextRefiner.toSentenceByStanfordNLP(str).get(2));
     }
 }
